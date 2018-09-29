192c0d196a551d5335ca04d5356057c27b688f90
Harry Phillips <harryjamesphillips@gmail.com>
2018-09-29 13:16:39 +0100

    Methods to obtain a BurstTimestamp as either a Java 7 or Java 8 time object

--- burstKit/src/main/java/burst/kit/entity/BurstTimestamp.java
+++ burstKit/src/main/java/burst/kit/entity/BurstTimestamp.java
@@ -1,21 +1,35 @@
 package burst.kit.entity;
 
+import brs.util.Convert;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializer;
 
-public class BurstTimestamp { // TODO Convert to Java time types
+import java.time.Instant;
+import java.util.Date;
 
-    public static final JsonDeserializer<BurstTimestamp> DESERIALIZER = (json, typeOfT, context) -> new BurstTimestamp(json.getAsLong());
+public class BurstTimestamp {
+
+    public static final JsonDeserializer<BurstTimestamp> DESERIALIZER = (json, typeOfT, context) -> new BurstTimestamp(json.getAsInt());
     public static final JsonSerializer<BurstTimestamp> SERIALIZER = (src, typeOfSrc, context) -> new JsonPrimitive(src.getTimestamp());
 
-    private long timestamp;
+    private final int timestamp;
+    private final Date date;
 
-    public BurstTimestamp(long rawTimestamp) {
+    public BurstTimestamp(int rawTimestamp) {
         this.timestamp = rawTimestamp;
+        this.date = Convert.fromEpochTime(timestamp + 1);
     }
 
     public long getTimestamp() {
         return timestamp;
+    }
+
+    public Date getAsDate() {
+        return date;
+    }
+
+    public Instant getAsInstant() {
+        return date.toInstant();
     }
 }
