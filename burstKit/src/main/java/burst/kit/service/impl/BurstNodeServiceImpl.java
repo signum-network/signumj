5dc7b83f808b452e0ca1b0a3cb5b2927d512d8ca
Harry Phillips <harryjamesphillips@gmail.com>
2019-01-05 15:02:17 +0000

    BurstCryptoImpl - Remove repeated code

--- burstKit/src/main/java/burst/kit/service/impl/BurstNodeServiceImpl.java
+++ burstKit/src/main/java/burst/kit/service/impl/BurstNodeServiceImpl.java
@@ -6,14 +6,11 @@ import burst.kit.util.BurstKitUtils;
 import burst.kit.util.SchedulerAssigner;
 
 import burst.kit.service.BurstNodeService;
-import com.google.gson.JsonObject;
 import io.reactivex.Single;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import retrofit2.http.GET;
-import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.*;
 
 public final class BurstNodeServiceImpl implements BurstNodeService {
 
