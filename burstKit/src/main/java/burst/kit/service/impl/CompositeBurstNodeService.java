a9f9cbcffd9c1d84d1cc3924ff7cd9ea02b86fa5
Harry Phillips <harryjamesphillips@gmail.com>
2019-07-02 10:16:20 +0100

    Layout for CompositeBurstNodeService

---
+++ burstKit/src/main/java/burst/kit/service/impl/CompositeBurstNodeService.java
@@ -0,0 +1,187 @@
+package burst.kit.service.impl;
+
+import burst.kit.entity.*;
+import burst.kit.entity.response.*;
+import burst.kit.service.BurstNodeService;
+import io.reactivex.Observable;
+import io.reactivex.Single;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+
+public class CompositeBurstNodeService implements BurstNodeService {
+    private final Collection<BurstNodeService> burstNodeServices;
+
+    public CompositeBurstNodeService(Collection<BurstNodeService> burstNodeServices) {
+        this.burstNodeServices = burstNodeServices;
+    }
+
+    private <T> Single<T> performFastest(Function<BurstNodeService, Single<T>> function) {
+        return null; // TODO
+    }
+
+    private <T> Observable<T> performFastestObservable(Function<BurstNodeService, Observable<T>> function) {
+        return null; // TODO
+    }
+
+    private <T> Single<T> performOnOne(Function<BurstNodeService, Single<T>> function) { // TODO remember failover! Do we have one priority node or what?
+        return null; // TODO
+    }
+
+    @Override
+    public Single<Block> getBlock(BurstID block) {
+        return performFastest(service -> service.getBlock(block));
+    }
+
+    @Override
+    public Single<Block> getBlock(int height) {
+        return performFastest(service -> service.getBlock(height));
+    }
+
+    @Override
+    public Single<Block> getBlock(BurstTimestamp timestamp) {
+        return performFastest(service -> service.getBlock(timestamp));
+    }
+
+    @Override
+    public Single<BurstID> getBlockId(int height) {
+        return performFastest(service -> service.getBlockId(height));
+    }
+
+    @Override
+    public Single<Block[]> getBlocks(int firstIndex, int lastIndex) {
+        return performFastest(service -> service.getBlocks(firstIndex, lastIndex));
+    }
+
+    @Override
+    public Single<Constants> getConstants() {
+        return performFastest(BurstNodeService::getConstants);
+    }
+
+    @Override
+    public Single<Account> getAccount(BurstAddress accountId) {
+        return performFastest(service -> service.getAccount(accountId));
+    }
+
+    @Override
+    public Single<AT[]> getAccountATs(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountATs(accountId));
+    }
+
+    @Override
+    public Single<BurstID[]> getAccountBlockIDs(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountBlockIDs(accountId));
+    }
+
+    @Override
+    public Single<Block[]> getAccountBlocks(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountBlocks(accountId));
+    }
+
+    @Override
+    public Single<BurstID[]> getAccountTransactionIDs(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountTransactionIDs(accountId));
+    }
+
+    @Override
+    public Single<Transaction[]> getAccountTransactions(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountTransactions(accountId));
+    }
+
+    @Override
+    public Single<BurstAddress[]> getAccountsWithRewardRecipient(BurstAddress accountId) {
+        return performFastest(service -> service.getAccountsWithRewardRecipient(accountId));
+    }
+
+    @Override
+    public Single<AT> getAt(BurstAddress at) {
+        return performFastest(service -> service.getAt(at));
+    }
+
+    @Override
+    public Single<BurstAddress[]> getAtIds() {
+        return performFastest(service -> service.getAtIds());
+    }
+
+    @Override
+    public Single<Transaction> getTransaction(BurstID transactionId) {
+        return performFastest(service -> service.getTransaction(transactionId));
+    }
+
+    @Override
+    public Single<Transaction> getTransaction(byte[] fullHash) {
+        return performFastest(service -> service.getTransaction(fullHash));
+    }
+
+    @Override
+    public Single<byte[]> getTransactionBytes(BurstID transactionId) {
+        return performFastest(service -> service.getTransactionBytes(transactionId));
+    }
+
+    @Override
+    public Single<byte[]> generateTransaction(BurstAddress recipient, byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline) {
+        return performFastest(service -> service.generateTransaction(recipient, senderPublicKey, amount, fee, deadline));
+    }
+
+    @Override
+    public Single<byte[]> generateTransactionWithMessage(BurstAddress recipient, byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline, String message) {
+        return performFastest(service -> service.generateTransactionWithMessage(recipient, senderPublicKey, amount, fee, deadline, message));
+    }
+
+    @Override
+    public Single<byte[]> generateTransactionWithMessage(BurstAddress recipient, byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline, byte[] message) {
+        return performFastest(service -> service.generateTransactionWithMessage(recipient, senderPublicKey, amount, fee, deadline, message));
+    }
+
+    @Override
+    public Single<byte[]> generateTransactionWithEncryptedMessage(BurstAddress recipient, byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline, BurstEncryptedMessage message) {
+        return performFastest(service -> service.generateTransactionWithEncryptedMessage(recipient, senderPublicKey, amount, fee, deadline, message));
+    }
+
+    @Override
+    public Single<byte[]> generateTransactionWithEncryptedMessageToSelf(BurstAddress recipient, byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline, BurstEncryptedMessage message) {
+        return performFastest(service -> service.generateTransactionWithEncryptedMessageToSelf(recipient, senderPublicKey, amount, fee, deadline, message));
+    }
+
+    @Override
+    public Single<FeeSuggestion> suggestFee() {
+        return performFastest(BurstNodeService::suggestFee);
+    }
+
+    @Override
+    public Observable<MiningInfo> getMiningInfo() {
+        return performFastestObservable(BurstNodeService::getMiningInfo);
+    }
+
+    @Override
+    public Single<TransactionBroadcast> broadcastTransaction(byte[] transactionBytes) {
+        return performFastest(service -> service.broadcastTransaction(transactionBytes));
+    }
+
+    @Override
+    public Single<BurstAddress> getRewardRecipient(BurstAddress account) {
+        return performFastest(service -> service.getRewardRecipient(account));
+    }
+
+    @Override
+    public Single<Long> submitNonce(String passphrase, String nonce, BurstID accountId) {
+        return performOnOne(service -> service.submitNonce(passphrase, nonce, accountId));
+    }
+
+    @Override
+    public Single<byte[]> generateMultiOutTransaction(byte[] senderPublicKey, BurstValue fee, int deadline, Map<BurstAddress, BurstValue> recipients) throws IllegalArgumentException {
+        return performFastest(service -> service.generateMultiOutTransaction(senderPublicKey, fee, deadline, recipients));
+    }
+
+    @Override
+    public Single<byte[]> generateMultiOutSameTransaction(byte[] senderPublicKey, BurstValue amount, BurstValue fee, int deadline, Set<BurstAddress> recipients) throws IllegalArgumentException {
+        return performFastest(service -> service.generateMultiOutSameTransaction(senderPublicKey, amount, fee, deadline, recipients));
+    }
+
+    @Override
+    public Single<byte[]> generateCreateATTransaction(byte[] senderPublicKey, BurstValue fee, int deadline, String name, String description, byte[] creationBytes) {
+        return performFastest(service -> service.generateCreateATTransaction(senderPublicKey, fee, deadline, name, description, creationBytes));
+    }
+}
