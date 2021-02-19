515558f71a887e2c9a12881cae02c9b161032561
jjos2372 <jjos2372@gmail.com>
2021-02-19 18:15:42 -0300

    Updating tests.

--- burstKit/src/test/java/burst/kit/test/HttpBurstNodeServiceTest.java
+++ burstKit/src/test/java/burst/kit/test/HttpBurstNodeServiceTest.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
-@Ignore // TODO
+// @Ignore // TODO
 public class HttpBurstNodeServiceTest extends BurstNodeServiceTest {
     @Override
     protected BurstNodeService getBurstNodeService() {
