e3c6abdfc53d8e0cb59d9311d6c1524d95e5b992
jjos2372 <jjos2372@gmail.com>
2023-04-16 13:24:03 -0300

    Cleanup warnings.

--- src/main/java/signumj/crypto/hash/HashProvider.java
+++ src/main/java/signumj/crypto/hash/HashProvider.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class HashProvider extends Provider {
 
     private static final String NAME = "SignumHashProvider";
-    private static final double VERSION = 1.0;
+    private static final String VERSION = "1.0";
     private static final String INFO = "A Shabal-256 MessageDigest provider";
 
     public static void init() {
